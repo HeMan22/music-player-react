38c4fd5c2c37ae4c122db36eb960fbe0624a07ad
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-10-09 23:27:02 +0530

    Added Sass stylings

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Player from './components/Player';
 import Song from './components/Song';
 
+// import styles
+import './styles/App.scss';
+
+
 function App() {
   return (
     <div className="App">
