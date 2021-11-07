2655390ddb625adadaf99eb53b157adf3bc4f1d3
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-11-07 23:59:34 +0530

    Alternate skip back

--- src/components/Player.js
+++ src/components/Player.js
@@ -25,6 +25,16 @@ const Player = ({ audioRef, currentSong, isPlaying, setIsPlaying, setSongInfo, s
 
   const skipTrackHandler = (direction) => {
     let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+    // if (direction === "skip-forward") {
+    //   // setCurrentSong(songs[currentIndex + 1]);
+    //   if (songs.length === currentIndex + 1) {
+    //     setCurrentSong(songs[0]);
+    //   }
+    //   else {
+    //     setCurrentSong(songs[currentIndex + 1]);
+    //     console.log(audioRef.current);
+    //   }
+    // }
     if (direction === "skip-forward") {
       // setCurrentSong(songs[currentIndex + 1]);
       if (songs.length === currentIndex + 1) {
